3680fc11fdf8c32befc93c827041672618a52bd2
Manoel Neto <manoel180@gmail.com>
2015-10-06 19:02:17 -0400

    update cards

--- app/src/main/java/br/com/cardmagic/DialogNextCard.java
+++ app/src/main/java/br/com/cardmagic/DialogNextCard.java
@@ -1,16 +1,15 @@
 package br.com.cardmagic;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.view.Window;
 import android.widget.Button;
 
 
 import roboguice.fragment.RoboDialogFragment;
-import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
 /**
@@ -27,8 +26,21 @@ public class DialogNextCard extends RoboDialogFragment {
     @InjectView(R.id.btnYes)
     private Button btnYes;
 
+    @InjectView(R.id.cards)
+    private ViewGroup gridLayout;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        this.setStyle(android.support.v4.app.DialogFragment.STYLE_NO_FRAME, R.style.dialog_default);
+        this.setCancelable(false);
+        super.onCreate( savedInstanceState );
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        this.getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+
         return inflater.inflate(R.layout.dialog_card, container,
                 false);
     }
@@ -37,7 +49,9 @@ public class DialogNextCard extends RoboDialogFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         init();
+
     }
+
 
     private void init() {
         btnCancel.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +65,7 @@ public class DialogNextCard extends RoboDialogFragment {
         btnYes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                gridLayout.inflate(getContext(),R.layout.card2,gridLayout);
             }
         });
     }
